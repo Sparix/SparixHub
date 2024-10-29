44576c2a0d28beac6e04f3cc13d0ffc047337eb8
Oleksandr Hontarenko <sparix@Mac.best.net>
2024-10-29 12:31:09 +0200

    create connections to mongo db

---
+++ db_connections.py
@@ -0,0 +1,26 @@
+import os
+
+from motor.motor_asyncio import AsyncIOMotorClient
+from contextlib import asynccontextmanager
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+@asynccontextmanager
+async def lifespan(app):
+    await startup_db_client(app)
+    yield
+    await shutdown_db_client(app)
+
+
+async def startup_db_client(app):
+    app.mongodb_client = AsyncIOMotorClient(
+        os.getenv("MONGO_URL")
+    )
+    app.mongodb = app.mongodb_client.get_database("college")
+
+
+async def shutdown_db_client(app):
+    app.mongodb_client.close()
