aba6055540698e98329c28b16025647de7fac324
Oleksandr Hontarenko <sparix@Mac.best.net>
2024-11-18 13:23:50 +0200

    add router for post

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 
 from db_connections import lifespan
 from authentication.authentication import auth_router
+from post.post import post_router
 from user_profile.profile import user_router
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(user_router, prefix="/user", tags=["user"])
+app.include_router(post_router, prefix="/post", tags=["post"])
 
 
 @app.get("/")
