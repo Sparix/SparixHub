17d7b69823ebdc291c5e1a89b98e927bef2b6afa
Oleksandr Hontarenko <sparix@Mac.best.net>
2024-12-23 12:38:41 +0200

    create add friend request to user, and view all request

--- user_profile/profile.py
+++ user_profile/profile.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 from typing import Annotated, Optional
 
 import jwt
+from bson import ObjectId
 from fastapi import APIRouter, Depends, HTTPException, status, Header
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import RedirectResponse
@@ -14,20 +16,22 @@ from authentication.authentication import (
 )
 from authentication.models import UserInDB
 from db_connections import db
-from user_profile.schemas import UserResponse
+from user_profile.models import FriendRequestDB
+from user_profile.schemas import UserResponse, FriendRequestSchema, FriendResponse, UserResponseFriends
 
 user_router = APIRouter()
 
 
-@user_router.get("/me", response_model=UserResponse)
+@user_router.get("/me", response_model=UserResponseFriends)
 async def read_users_me(
         current_user: Annotated[UserInDB, Depends(get_current_active_user)],
 ):
-    return UserResponse(
+    return UserResponseFriends(
         username=current_user.username,
         email=current_user.email,
         first_name=current_user.first_name,
         last_name=current_user.last_name,
+        friend_list=current_user.friend_list,
     )
 
 
@@ -58,11 +62,12 @@ async def get_user_by_username(
     if current_user and current_user.username.lower() == username.lower():
         return RedirectResponse(url="/user/me")
 
-    return UserResponse(
+    return UserResponseFriends(
         username=obj_user["username"],
         email=obj_user["email"],
         first_name=obj_user["first_name"],
         last_name=obj_user["last_name"],
+        friend_list=obj_user["friend_list"],
     )
 
 
@@ -71,7 +76,6 @@ async def update_user_profile(
         profile_data: UserResponse,
         current_user: Annotated[UserInDB, Depends(get_current_active_user)]
 ):
-    print("user: ", profile_data)
     update_data = profile_data.dict(exclude_unset=True)
     if not update_data:
         raise HTTPException(
@@ -89,3 +93,112 @@ async def update_user_profile(
     )
 
     return RedirectResponse(url="/user/me")
+
+
+@user_router.post("/friends/request/")
+async def send_friend_request(
+    friend_request: FriendRequestSchema,
+    current_user: Annotated[UserInDB, Depends(get_current_active_user)],
+):
+    receiver_username = friend_request.friend_username
+    existing_request = await db["friend_requests"].find_one({
+        "sender_username": current_user.username,
+        "receiver_username": receiver_username
+    })
+    if existing_request:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Friend request already sent"
+        )
+
+    if receiver_username in current_user.friend_list:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="You already connected with this friend"
+        )
+
+    friend_request_in_db = {
+        "sender_username": current_user.username,
+        "receiver_username": receiver_username,
+        "status": "pending",
+        "created_at": datetime.utcnow()
+    }
+
+    await db["friend_requests"].insert_one(friend_request_in_db)
+    return {"message": "Friend request sent!"}
+
+@user_router.post("/friends/accept/")
+async def accept_friend_request(
+    friend_request: FriendRequestSchema,
+    current_user: Annotated[UserInDB, Depends(get_current_active_user)],
+):
+    sender_username = friend_request.friend_username
+
+    friend_request_db = await db["friend_requests"].find_one({
+        "sender_username": sender_username,
+        "receiver_username": current_user.username
+    })
+    if not friend_request_db:
+        raise HTTPException(status_code=404, detail="Friend request not found")
+
+    await db["friend_requests"].update_one(
+        {"sender_username": sender_username, "receiver_username": current_user.username},
+        {"$set": {"status": "accepted"}}
+    )
+
+    await db["users"].update_one(
+        {"username": sender_username},
+        {"$addToSet": {"friend_list": current_user.username}}
+    )
+    await db["users"].update_one(
+        {"username": current_user.username},
+        {"$addToSet": {"friend_list": sender_username}}
+    )
+
+    return {"message": "Friend request accepted!"}
+
+@user_router.get("/friends/request/list/")
+async def list_friend_requests(
+    current_user: Annotated[UserInDB, Depends(get_current_active_user)],
+):
+    obj_friend_requests = await db["friend_requests"].find({
+        "receiver_username": current_user.username,
+        "status": "pending"
+    }).sort("created_at", 1).to_list(length=None)
+
+    if not obj_friend_requests:
+        return {"message": "You don't have any friend request!"}
+
+    friend_requests = list()
+
+    for obj_friend_request in obj_friend_requests:
+        sender_username = obj_friend_request["sender_username"]
+        receiver_username = obj_friend_request["receiver_username"]
+
+        sender = await db["users"].find_one({"username": sender_username})
+        receiver = await db["users"].find_one({"username": receiver_username})
+
+        if not sender or not receiver:
+            continue
+
+        sender_response = UserResponse(
+            username=sender["username"],
+            email=sender.get("email"),
+            first_name=sender.get("first_name"),
+            last_name=sender.get("last_name")
+        )
+
+        receiver_response = UserResponse(
+            username=receiver["username"],
+            email=receiver.get("email"),
+            first_name=receiver.get("first_name"),
+            last_name=receiver.get("last_name")
+        )
+
+        friend_requests.append(FriendResponse(
+            sender=sender_response,
+            receiver=receiver_response,
+            created_at=obj_friend_request["created_at"],
+        ))
+
+    return friend_requests
--- user_profile/schemas.py
+++ user_profile/schemas.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class UserResponse(BaseModel):
@@ -7,3 +8,14 @@ class UserResponse(BaseModel):
     email: Optional[str] = None
     first_name: Optional[str] = None
     last_name: Optional[str] = None
+
+class UserResponseFriends(UserResponse):
+    friend_list: Optional[list] = None
+
+class FriendRequestSchema(BaseModel):
+    friend_username: str
+
+class FriendResponse(BaseModel):
+    sender: UserResponse
+    receiver: UserResponse
+    created_at: datetime = Field(default_factory=datetime.utcnow)
