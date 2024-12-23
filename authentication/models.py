c917336aec6f012b3df6ffdfd6641d2f9b7e75e7
Oleksandr Hontarenko <sparix@Mac.best.net>
2024-12-23 12:37:43 +0200

    update UserInDB add friend list

--- authentication/models.py
+++ authentication/models.py
@@ -10,6 +10,7 @@ class UserInDB(BaseModel):
     first_name: Optional[str] = None
     last_name: Optional[str] = None
     hashed_password: str
+    friend_list: Optional[list] = Field(default_factory=list)
 
     class Config:
         arbitrary_types_allowed = True
